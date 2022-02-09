d3d017ddaed812c6921834a7cd269cd60275bdb3
XI3394-ingitsinghal <99240023+XI3394-ingitsinghal@users.noreply.github.com>
2022-02-09 19:08:26 +0530

    Update Sample_calculator.java

--- sample-programs/Sample_calculator.java
+++ sample-programs/Sample_calculator.java
@@ -1,15 +1,15 @@
 //sample calculator(MADS)
-import java.io.*;
+
 import java.util.Scanner;
 
-class Sample_calculator 
+class Calculator 
 {
        
       public static void main(String args[])
       {
-         // intialisation of variables
-         int n1=0,n2=0;
-         char operator = '/0';    // initialising char variable as null
+         // Initialization of variables
+         int n1=0,n2=0,answer;
+         char operator;    //Initializing char variable as null
          
          // creating object for scanner class
          Scanner s = new Scanner(System.in);
@@ -25,54 +25,32 @@ class Sample_calculator
          n2 = s.nextInt();
          
          // creating object to access public methods of class
-         Sample_calculator m = new Sample_calculator();
+         Calculator m = new Calculator();
          
          switch(operator)
          {
             case '+':
-            System.out.println("Answer= " + m.addition(n1,n2));
+            System.out.println("Answer= " + (n1+n2));
             break;
             
             case '-':
-            System.out.println("Answer= " + m.sub(n1,n2));
+            System.out.println("Answer= " + (n1-n2));
             break;
             
             case '*':
-            System.out.println("Answer= " + m.mul(n1,n2));
+            System.out.println("Answer= " + (n1*n2));
             break;
             
             case '/':
-            System.out.println("Answer= " + m.div(n1,n2));
+            System.out.println("Answer= " + (n1/n2));
             break;
+            
+            default:
+            	System.out.println("Wrong Operator");
+            	break;
          } //end of switch case
          
-      // Method for addition   
-      public int addition(int n1, int n2) 
-      {
-      answer= n1 + n2;
-      return answer;
-      }
-      
-      // Method for substraction
-      public int sub(int n1, int n2) 
-      {
-      answer= n1 - n2;
-      return answer;
-      }
-      
-      //Method for multiplication
-      public int mul(int n1, int n2) 
-      {
-      answer= n1 * n2;
-      return answer;
-      }
-      
-      //Method for divison
-      public int div(int n1, int n2) 
-      {
-      answer= n1 / n2;
-      return answer;
-      }
+    
     
     } //end of main method
     
