367ab452143c9e7a1a25828d2302c6f4a4ec3a8c
Ingit.Singhal <Ingit.Singhal@XE-GGN-IT-01691.XEBIAINDIA.LOCAL>
2022-02-11 16:13:55 +0530

    Bubble, selection, quick sort

---
+++ Sorting Algorithms/src/QuickSort.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class QuickSort {
+	
+	// Partition into low and high index
+	int partition(int arr[], int lowIndex, int highIndex)
+	{
+	int pivot = arr[highIndex];
+	int i = (lowIndex-1);
+	for (int j=lowIndex; j<highIndex; j++)
+	{
+	if (arr[j] <= pivot)
+	{
+	i++;
+	int temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+	}
+	}
+	int temp = arr[i+1];
+	arr[i+1] = arr[highIndex];
+	arr[highIndex] = temp;
+	return i+1;
+	}
+	
+	// Sorting method
+	void sort(int arr[], int lowIndex, int highIndex)
+	{
+	if (lowIndex < highIndex)
+	{
+	int pi = partition(arr, lowIndex, highIndex);
+	sort(arr, lowIndex, pi-1);
+	sort(arr, pi+1, highIndex);
+	}
+	}
+	
+	static void printArray(int arr[])
+	{
+	int n = arr.length;
+	for (int i=0; i<n; ++i)
+	System.out.print(arr[i]+" ");
+	System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		int arr[] = new int[5];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 5 no.s ");
+        for(int i=0;i<=4;i++) {
+        	arr[i]= sc.nextInt();
+        }
+	int n = arr.length;
+	QuickSort ob = new QuickSort();
+	ob.sort(arr, 0, n-1);
+	System.out.println("sorted array");
+	printArray(arr);
+	}
+}	
+	
---
+++ Sorting Algorithms/src/SelectionSort.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+	
+	void selectionSort(int arr[]) 
+    { 
+        int pos;
+        int temp;
+        for (int i = 0; i < arr.length; i++) 
+        { 
+            pos = i; 
+            for (int j = i+1; j < arr.length; j++) 
+           {
+                if (arr[j] < arr[pos])   //find the index of the minimum element
+                {
+                    pos = j;
+                }
+            }
+
+            temp = arr[pos];            //swap the current element with the minimum element
+            arr[pos] = arr[i]; 
+            arr[i] = temp; 
+        } 
+    } 
+	
+	void display(int arr[])                     //display the array
+    { 
+        for (int i=0; i<arr.length; i++) 
+        {
+            System.out.print(arr[i]+" ");
+        }  
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        SelectionSort ob = new SelectionSort(); 
+        int arr[] = new int[5];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 5 no.s ");
+        for(int i=0;i<=4;i++) {
+        	arr[i]= sc.nextInt();
+        }
+        ob.selectionSort(arr); 
+        ob.display(arr); 
+    } 
+}
