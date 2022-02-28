510c2c8707209b7543389192d05c024e13574f2f
XI3394-ingitsinghal <99240023+XI3394-ingitsinghal@users.noreply.github.com>
2022-02-28 15:37:00 +0530

    Update AbstractMain.java

--- Object Oriented Programming/AbstractMain.java
+++ Object Oriented Programming/AbstractMain.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class AbstractMain {
 	public static void main(String []args){
-		//Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
+		//Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract;
+		//cannot be instantiated
 		Scanner sc=new Scanner(System.in);
 		String title=sc.nextLine();
 		MyBook new_novel=new MyBook();
 		new_novel.setTitle(title);
 		System.out.println("The title is: "+new_novel.getTitle());
-      	sc.close();
+      	        sc.close();
 		
 	}
 
